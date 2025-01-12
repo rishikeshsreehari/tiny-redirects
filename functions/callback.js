f9dac6c9ba293a38dc1fda6ff9bbd07ec7f394be
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2025-01-12 23:36:26 +0400

    debugging

--- functions/callback.js
+++ functions/callback.js
@@ -1,36 +1,45 @@
 // functions/callback.js
 export async function onRequest(context) {
-    const code = new URL(context.request.url).searchParams.get("code");
-    
-    // Exchange code for token
-    const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        client_id: context.env.GITHUB_CLIENT_ID,
-        client_secret: context.env.GITHUB_CLIENT_SECRET,
-        code: code,
-      }),
-    });
+    try {
+      // Log the incoming request URL
+      console.log("Callback URL:", context.request.url);
+      
+      const code = new URL(context.request.url).searchParams.get("code");
+      console.log("Received code:", code);
   
-    const data = await tokenResponse.json();
-    
-    // Return the token to the CMS
-    return new Response(
-      `<script>
-        window.opener.postMessage(
-          'authorization:github:success:${JSON.stringify(data)}',
-          '*'
-        );
-        window.close();
-      </script>`,
-      {
+      const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
         headers: {
-          "Content-Type": "text/html",
+          "Accept": "application/json",
+          "Content-Type": "application/json",
         },
-      }
-    );
+        body: JSON.stringify({
+          client_id: context.env.GITHUB_CLIENT_ID,
+          client_secret: context.env.GITHUB_CLIENT_SECRET,
+          code: code,
+        }),
+      });
+  
+      const data = await tokenResponse.json();
+      console.log("Token response:", data);
+  
+      return new Response(
+        `<script>
+          console.log("Sending message to opener");
+          window.opener.postMessage(
+            'authorization:github:success:${JSON.stringify(data)}',
+            'https://r1l.in'
+          );
+          window.close();
+        </script>`,
+        {
+          headers: {
+            "Content-Type": "text/html",
+          },
+        }
+      );
+    } catch (error) {
+      console.error("Error in callback:", error);
+      return new Response(`Error: ${error.message}`, { status: 500 });
+    }
   }
